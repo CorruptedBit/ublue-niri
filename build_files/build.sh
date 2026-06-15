b1bc695d40b73e635cc658f575a2cd9506af9257
CorruptedBit <102173283+CorruptedBit@users.noreply.github.com>
2026-06-15 22:08:18 +0200

    add just file and some dev tools

--- build_files/build.sh
+++ build_files/build.sh
@@ -33,13 +33,27 @@ rpm --import https://download.docker.com/linux/fedora/gpg
 dnf5 config-manager addrepo --from-repofile=https://download.docker.com/linux/fedora/docker-ce.repo
 dnf5 install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 
+# VsCode from Microsoft
+rpm --import https://packages.microsoft.com/keys/microsoft.asc
 
-# Use a COPR Example:
-#
-# dnf5 -y copr enable ublue-os/staging
-# dnf5 -y install package
-# Disable COPRs so they don't end up enabled on the final image:
-# dnf5 -y copr disable ublue-os/staging
+cat << 'EOF' > /etc/yum.repos.d/vscode.repo
+[code]
+name=Visual Studio Code
+baseurl=https://packages.microsoft.com/yumrepos/vscode
+enabled=1
+autorefresh=1
+type=rpm-md
+gpgcheck=1
+gpgkey=https://packages.microsoft.com/keys/microsoft.asc
+EOF
+
+dnf5 install -y code
+
+# Terra Software
+dnf5 config-manager setopt terra.enabled=1
+dnf5 install -y zed
+dnf5 config-manager setopt terra.enabled=0
+
 
 #### Example for enabling a System Unit File
 
